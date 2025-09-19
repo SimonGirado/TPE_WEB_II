881f67ec65710e4655871dd744a5ad501a355716
SimonGirado <145472376+SimonGirado@users.noreply.github.com>
2025-09-19 11:31:08 -0300

    .

--- peliculas_pag.sql
+++ peliculas_pag.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-09-2024 a las 21:32:55
+-- Tiempo de generación: 17-09-2025 a las 21:32:55
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
